a34b660a7d0ef189b8251fbbab7e31f72d536639
Anurag Grover <grover.anurag87@gmail.com>
2019-07-08 12:52:13 +0530

    master 2

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource master');
+  res.send('respond with a resource master 2');
 });
 
 module.exports = router;
