d9aef7e4872b904b99d90810c50f72c93c258ac2
Anurag Grover <grover.anurag87@gmail.com>
2019-07-08 12:52:48 +0530

    feature 3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express 2 and feature' });
+  res.render('index', { title: 'Express 3 and feature' });
 });
 
 module.exports = router;
